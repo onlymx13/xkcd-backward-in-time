b70215ff295b4314848b57484093aa1ae05b0a32
onlymx13 <37835222+onlymx13@users.noreply.github.com>
2024-06-25 23:24:47 -0500

    (edit git history to remove assets) and earth images working!

--- index.ts
+++ index.ts
@@ -8,6 +8,9 @@ const MIN_PER_HR = 60;
 const HR_PER_DAY = 24;
 const SEC_PER_DAY = SEC_PER_MIN * MIN_PER_HR * HR_PER_DAY;
 const DAYS_PER_YR = 365.2425;
+const MS_PER_YR = MS_PER_SEC * SEC_PER_DAY * DAYS_PER_YR;
+
+import { intervalToDuration, Duration, differenceInDays, differenceInHours, differenceInMinutes, differenceInSeconds, formatDuration, addDays, yearsToDays, Interval } from "date-fns";
 
 /*
 BigInt absolute value
@@ -80,44 +83,160 @@ class MyDate {
     }
 }
 
-function updateHtmlFromValues(ms: bigint): void {
+function updateRemainingTime(days: number, hours: number, minutes: number, seconds: number): void {
     const remainingDaysElement = document.getElementById("remaining-days")!;
     const remainingHoursElement = document.getElementById("remaining-hours")!;
     const remainingMinutesElement = document.getElementById("remaining-minutes")!;
     const remainingSecondsElement = document.getElementById("remaining-seconds")!;
 
-    dateFns.intervalToDuration({
-        start: new Date(1929, 0, 15, 12, 0, 0),
-        end: new Date(1968, 3, 4, 19, 5, 0)
-      })
-
+    remainingDaysElement.textContent = days.toString();
+    remainingHoursElement.textContent = hours.toString();
+    remainingMinutesElement.textContent = minutes.toString();
+    remainingSecondsElement.textContent = seconds.toString();
 }
 
-const startDate = MyDate.fromDate(new Date(Date.UTC(2024, 5, 22, 12, 57))); // June 22, 2024 12:57:00 UTC
+function updateXkcdDate(date: MyDate): void {
+    document.getElementById("date")!.textContent = date.toString();
+}
 
-const endDate = MyDate.fromDate(new Date(Date.UTC(2025, 0, 10, 0, 17))); // January 10, 2024 00:17:00 UTC
+function updateTimeAgo(timeAgo: string): void {
+    document.getElementById("time-ago")!.textContent = timeAgo;
+}
 
-const now = MyDate.fromDate(new Date());
+let lastClosestImage = -1; // TODO: cringe af
 
-const big = Number.MAX_SAFE_INTEGER;
-// Calculate p, the percentage of the time that we have made it through so far
-// Assuming we are in the timeframe specified,
-// p is an ordinary number in [0, 1].
-const p = Number(MyDate.msBetween(startDate, now) * BigInt(big) / MyDate.msBetween(startDate, endDate)) / big;
+function mya_to_image_number(mya: number): number {
+    if (mya >= 3600) {
+        return 1 + (4540 - mya) / 0.5;
+    } else {
+        return mya_to_image_number(3600) + (3600 - mya) / 0.2;
+    }
+}
 
-// Calculate T from the formula in the xkcd comic.
-// T is a positive value in years, according to the comic.
-const T = Math.exp(20.3444 * (p * p * p) + 3) - Math.exp(3);
-// Since years don't really make sense here, we convert this value to ms.
-const T_ms = BigInt(Math.round(T * DAYS_PER_YR * SEC_PER_DAY * MS_PER_SEC));
+function updateEarthImage(yearsAgo: number): void {
+    const earthImage = document.getElementById("earth-image")! as HTMLImageElement;
+    const mya = yearsAgo / 1_000_000;
+    if (mya > 4540) {
+        if (lastClosestImage !== -2) {
+            //TODO Handle the Earth not existing
+            lastClosestImage = -2;
+            earthImage.src = "";
+            //throw(new RangeError("Tried to get image of an Earth that does not exist"));
+        }
+    } else {
+        const desired_image_number = mya_to_image_number(mya);
+        const nearest_legal_image = Math.round(desired_image_number); // find whichever image comes from the nearest time to our time
 
-console.log(
-`
-    start: ${startDate}
-    end: ${endDate}
-    now: ${now}
-    p = ${p}
-    T = ${T} years
-      = ${T_ms} ms
-    curr = ${now.addMs(-T_ms)}
-`);
+        if (nearest_legal_image !== lastClosestImage) {
+            earthImage.src = `assets/earth_images/img${nearest_legal_image}.jpg`;
+            lastClosestImage = nearest_legal_image;
+        }
+    }
+}
+const startDate = new Date(Date.UTC(2024, 5, 22, 12, 57)); // June 22, 2024 12:57:00 UTC
+
+const endDate = new Date(Date.UTC(2025, 0, 10, 0, 17)); // January 10, 2025 00:17:00 UTC
+
+let lastInterval: NodeJS.Timeout | undefined;
+let getNow = function() : Date {
+    nowSlider.value = (new Date()).getTime().toString();
+    return new Date;
+};
+const nowSlider = document.getElementById("set-now")! as HTMLInputElement;
+nowSlider.min = startDate.getTime().toString();
+nowSlider.max = endDate.getTime().toString();
+nowSlider.value = (new Date()).getTime().toString();
+
+(document.getElementById("use-set-now")! as HTMLInputElement).addEventListener("change", function() {
+    if (this.checked) {
+        getNow = () => new Date(Number(nowSlider.value));
+        if (lastInterval) {
+            clearInterval(lastInterval);
+        }
+        lastInterval = setInterval(() => nowSlider.value = (Number(nowSlider.value) + 100).toString(), 100);
+    } else {
+        if (lastInterval) {
+            clearInterval(lastInterval);
+        }
+        getNow = function() {
+            nowSlider.value = (new Date()).getTime().toString();
+            return new Date;
+        }
+    }
+});
+
+function formatNumberOfYears(num: number): string {
+    if (num < 1_000) {
+        return `${num} years`;
+    } else if (num < 1_000_000) {
+        return `${Math.floor(num / 1000)},${(num % 1000).toString().padStart(3, '0')} years`;
+    } else if (num < 1_000_000_000) {
+        return `${num / 1_000_000} million years`;
+    } else {
+        return `${num / 1_000_000_000} billion years`;
+    }
+}
+
+function update() {
+    const now = getNow();
+
+    // Calculate p, the percentage of the time that we have made it through so far
+    // Assuming we are in the timeframe specified,
+    // p is an ordinary number in [0, 1].
+    const p = (now.getTime() - startDate.getTime()) / (endDate.getTime() - startDate.getTime());
+
+    // Calculate T from the formula in the xkcd comic.
+    // T is a positive value in years, according to the comic.
+    const T = Math.exp(20.3444 * (p * p * p) + 3) - Math.exp(3);
+    // Since years don't really make sense here, we convert this value to ms.
+    //! TODO: won't this lose a lot of precision as written? do better
+    // since T is a decimal
+    const T_ms = BigInt(Math.round(T * DAYS_PER_YR * SEC_PER_DAY * MS_PER_SEC));
+
+    const xkcdDate = MyDate.fromDate(now).addMs(-T_ms);
+
+    /*
+    console.log(
+    `
+        start: ${startDate}
+        end: ${endDate}
+        now: ${now}
+        p = ${p}
+        T = ${T} years
+        = ${T_ms} ms
+        curr = ${xkcdDate}
+    `);
+    */
+
+    const days = differenceInDays(endDate, now);
+    const hours = differenceInHours(endDate, now) % HR_PER_DAY; // TODO if there is DST going on this fucks shit up, see docs for differenceInFullDays
+    const minutes = differenceInMinutes(endDate, now) % MIN_PER_HR;
+    const seconds = differenceInSeconds(endDate, now) % SEC_PER_MIN;
+
+    /*
+    Calculate the display that shows how long ago the xkcd date is, relative to now.
+    */
+    let timeAgo: string;
+    try {
+        timeAgo = `${formatDuration(intervalToDuration({
+            start: xkcdDate.toDate(),
+            end: now
+        }))} ${(now.getTime() - xkcdDate.toDate().getTime()) % 1000} milliseconds ago`;
+    } catch(e) {
+        if (e instanceof RangeError) {
+            // date is too long ago to use ordinary Dates
+            timeAgo = `${formatNumberOfYears(T)} ago`;
+        } else {
+            throw(e);
+        }
+    }
+
+    updateRemainingTime(days, hours, minutes, seconds);
+    updateXkcdDate(xkcdDate);
+    updateTimeAgo(timeAgo);
+    updateEarthImage(T);
+
+    requestAnimationFrame(update);
+}
+
+requestAnimationFrame(update);
---
+++ tools/build_assets.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/sh
+# Earth images are from https://www.youtube.com/watch?v=Q1OreyX0-fw, which is licensed under CC BY (https://creativecommons.org/licenses/by/4.0/).
+#TODO: what if that video goes down?
+outdir=assets/earth_images
+mkdir -p "$outdir"
+if [ $(ls -1 "$outdir" | wc -l) -eq 19881 ] && [ "$1" != "-f" ]
+then
+    echo "Assets already present in $outdir (pass -f to re-extract them anyway)."
+else
+    rm -rf "$outdir/*"
+    # Download the relevant section of the video
+    #TODO can this be done in one pass? (without --exec)
+    yt-dlp https://www.youtube.com/watch?v=Q1OreyX0-fw --download-sections "*18-11:20.7" --exec "ffmpeg -i {} $outdir/img%d.jpg"
+    # Cut out the first 240 frames and adjust everything to match
+    # (necessary unless we use --force-keyframes-at-cuts in ffmpeg, which makes it SO SLOW)
+    ls -v $outdir | cat -n |
+    while read num filename
+    do
+        if [ $num -ge 241 ]
+        then
+            mv -i -- "$outdir/$filename" "$outdir/img$(($num - 240)).jpg"
+        else
+            rm "$outdir/$filename"
+        fi
+    done
+    # Delete intermediate file
+    rm -f History\ of\ the\ Earth\ \[Q1OreyX0-fw\].webm
+fi
